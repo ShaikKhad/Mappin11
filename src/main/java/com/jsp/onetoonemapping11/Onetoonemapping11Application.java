b571a235405d07796fc75571ab9fe86fe240ef1c
Shaik Mahaboob Basha <basha.998566@gmail.com>
2024-08-16 18:38:08 +0530

    commit

---
+++ src/main/java/com/jsp/onetoonemapping11/Onetoonemapping11Application.java
@@ -0,0 +1,13 @@
+package com.jsp.onetoonemapping11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Onetoonemapping11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Onetoonemapping11Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/onetoonemapping11/Onetoonemapping11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.onetoonemapping11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Onetoonemapping11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
